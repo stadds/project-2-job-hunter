edcf7d020c14fb7ee375bc7e48a02481179865e5
STadds <staddi@gmail.com>
2020-05-16 10:18:18 -0400

    changed const sequelize to var sequelize

--- models/index.js
+++ models/index.js
@@ -9,9 +9,9 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 if (config.use_env_variable) {
-  const sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  const sequelize = new Sequelize(
+  var sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
