a64f14e17af350d74690059b724cf6c628cc746b
STadds <staddi@gmail.com>
2020-05-17 16:49:38 -0400

    added company, location, job type (full/part time)

--- models/savedjob.js
+++ models/savedjob.js
@@ -5,45 +5,46 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 255]
-      }
+        len: [1, 255],
+      },
     },
     description: {
       type: DataTypes.TEXT,
       validate: {
-        len: [1]
-      }
+        len: [1],
+      },
     },
     url: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true
-      }
+        notEmpty: true,
+      },
     },
+    location: { type: DataTypes.STRING },
+    company: { type: DataTypes.STRING },
+    type: { type: DataTypes.STRING },
     applied: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
     },
     userid: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true
-      }
+        notEmpty: true,
+      },
     },
   });
 
-  SavedJob.associate = function (models){
-    SavedJob.hasMany(models.Todo,{
-      onDelete: "cascade"
-
+  SavedJob.associate = function (models) {
+    SavedJob.hasMany(models.Todo, {
+      onDelete: "cascade",
     });
   };
 
   return SavedJob;
 };
-  
 
 // CREATE TABLE saved_jobs{
 //     id INT NOT NULL ,
