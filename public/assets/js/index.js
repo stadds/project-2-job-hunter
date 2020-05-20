8f3ae0f7ddec9c0da2b519757a5cd785bde47058
STadds <staddi@gmail.com>
2020-05-19 20:37:35 -0400

    stubbed submit event

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,14 @@
 //to hold the 3rd party search api call
 $(document).ready(function(){
 
+    $("#search-btn").on("submit", function(event){
+
+        event.preventDefault();
+
+        
+
+
+
+    });
+
 });
